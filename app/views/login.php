1bfaa533a47a5d8c3e49452b1769700c1d4fb031
Cikica <aleksandar.winter@gmail.com>
2014-08-17 17:09:43 +0100

    Added the login view

---
+++ app/views/login.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8">
+	<title>DotaStrat</title>
+	<script 
+		data-directory="/js/login" 
+		src="/js/login/entry.js">
+	</script>
+	<link rel="stylesheet" type="text/css" href="/css/login/main.css">
+</head>
+<body>
+	<!-- <div id="login" class="body"></div> -->
+	<script>
+	</script>
+</body>
+</html>
